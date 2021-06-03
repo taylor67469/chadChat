414b528dbb63917107a28fa1dd3ccfbae5b5999e
Zachary <taylor67469@yahoo.com>
2021-06-03 14:56:48 -0500

    Forum post layout done so far

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Forum from "./pages/forum"
 import Signup from "./pages/signup"
+import ForumPost from "./pages/forumPost"
 function App() {
   return (
     <Router>
     <div>
-      <h1>Hello world!</h1>
       <Switch>
         <Route exact path={"/"}>
             <Forum ></Forum>
         </Route>
         <Route exact path={"/signup"}>
           <Signup></Signup>
+        </Route>
+        <Route exact path={"/forumpost"}>
+          <ForumPost></ForumPost>
         </Route>
       </Switch>
     </div>
