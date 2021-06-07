481a633c3e506e5f340f8266baacb32a08f08379
Angel Flores <angelfloreschicago@gmail.com>
2021-06-07 12:19:01 -0500

    navbar displaying on other pages

--- client/src/pages/navbar.js
+++ client/src/pages/navbar.js
@@ -8,23 +8,26 @@ function Navbar() {
       <br/>
     <nav className="navbar navbar-expand-lg navbar-light navstyle">
     <div className="container-fluid">
-      <a className="nav-link active navtext" href="/some/valid/uri">ChadChat</a>
+      <a className="nav-link active navtext" href="/">ChadChat</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link navtext" aria-current="page" href="/some/valid/uri">Home</a>
+            <a className="nav-link navtext" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link navtext" href="/some/valid/uri">Features</a>
+            <a className="nav-link navtext" href="/some/valid/uri">Chat Room</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link navtext" href="/some/valid/uri">Pricing</a>
+            <a className="nav-link navtext" href="/some/valid/uri">Forum</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link navtext" href="/some/valid/uri" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a className="nav-link navtext" href="/login" tabindex="-1" aria-disabled="true">Login</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link navtext" href="/signup" tabindex="-1" aria-disabled="true">Signup</a>
           </li>
         </ul>
       </div>
