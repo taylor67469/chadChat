f9ac0c572e2e6b0fc2ca2a40828e10fb3c55141b
Angel Flores <angelfloreschicago@gmail.com>
2021-06-08 13:48:51 -0500

    Login Form CSS updates

--- client/src/component/log.js
+++ client/src/component/log.js
@@ -35,28 +35,63 @@ const Login=()=>{
   };
     // Notice how each input has a `value`, `name`, and `onChange` prop
     return (
-      <div>
-        <p>
-          Hello {login.username}
-        </p>
-        <form className="form">
-          <input
-            value={login.username}
-            name="firstName"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="Username"
-          />
-          <input
-            value={login.password}
-            name="password"
-            onChange={handleInputChange}
-            type="password"
-            placeholder="password"
-          />
-          <button onClick={handleFormSubmit}>Submit</button>
-        </form>
-      </div>
+      // <div>
+      //   <p>
+      //     Hello {login.username}
+      //   </p>
+      //   <form className="form">
+      //     <input
+      //       value={login.username}
+      //       name="firstName"
+      //       onChange={handleInputChange}
+      //       type="text"
+      //       placeholder="Username"
+      //     />
+      //     <input
+      //       value={login.password}
+      //       name="password"
+      //       onChange={handleInputChange}
+      //       type="password"
+      //       placeholder="password"
+      //     />
+      //     <button onClick={handleFormSubmit}>Submit</button>
+      //   </form>
+      // </div>
+
+      <div className="container login-container">
+            <div className="row justify-content-center">
+                <div className="col-7 login-form-1" style={{textAlign: "center"}}>
+                    <h3>Login to ChadChat</h3>
+                    <form>
+                        <div className="form-group">
+                            <input className="form-control" 
+                            value={login.username}
+                            name="firstName"
+                            onChange={handleInputChange}
+                            type="text"
+                            placeholder="Your Email *" />
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control"
+                            value={login.password}
+                            name="password"
+                            onChange={handleInputChange}
+                            type="password"
+                            placeholder="Your Password *" 
+                            />
+                        </div>
+                        <div className="form-group">
+                            {/* <input type="submit" className="btnSubmit" value="Login" /> */}
+                            <button className="btnSubmit" 
+                            onClick={handleFormSubmit}>Login</button>
+                        </div>
+                        <div className="form-group">
+                            <a href="/signup" className="orSign">or SignUp?</a>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
     );
   }
 
