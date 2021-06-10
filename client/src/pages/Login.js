c7a24919433a6223ee70044a546662fe42be8054
Zachary <taylor67469@yahoo.com>
2021-06-10 10:57:44 -0500

    grabs forum data and user to forumpost page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import LoginSheet from "../component/log.js";
 
 function LoginPage(props) {
-  return <LoginSheet userLoggedIn={props.userLoggedIn}/>;
+  return <LoginSheet userLoggedIn={props.userLoggedIn} useForumUser={props.useForumUser}/>;
 }
 
 export default LoginPage;
