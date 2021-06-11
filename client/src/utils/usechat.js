532a784176022eddc9187f7724b1163d0223d8e3
Zachary <taylor67469@yahoo.com>
2021-06-11 11:42:56 -0500

    working chat please

--- client/src/utils/usechat.js
+++ client/src/utils/usechat.js
@@ -1,14 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
+import API from './API';
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage"; // Name of the event
 const SOCKET_SERVER_URL = "https://chadchat1234.herokuapp.com";
+// const SOCKET_SERVER_URL = "http://localhost:3000";
 
 const useChat = (roomId) => {
   // console.log(roomId);
   const [messages, setMessages] = useState([]); // Sent and received messages
   const socketRef = useRef();
   useEffect(() => {
+
     // Creates a WebSocket connection
     socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
       query: { roomId },
@@ -34,14 +37,15 @@ const useChat = (roomId) => {
 
   // Sends a message to the server that
   // forwards it to all users in the same room
-  const sendMessage = (messageBody) => {
+  const sendMessage = (messageBody,user) => {
     socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
       body: messageBody,
       senderId: socketRef.current.id,
+      user
     });
   };
 
-  return { messages, sendMessage };
+  return { messages, sendMessage,setMessages };
 };
 
 export default useChat;
